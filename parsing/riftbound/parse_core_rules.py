e2bebb00b90e2765cfefca253d92340f04f3e7d9
Villawhatever <andrew@primefour.com>
2026-03-30 13:46:47 -0500

    CR/TR updates; parser tweaks to pass args for dry runs

--- parsing/riftbound/parse_core_rules.py
+++ parsing/riftbound/parse_core_rules.py
@@ -1,28 +1,40 @@
+import argparse
 import firebase_admin
 from firebase_admin import credentials
 from google.cloud import firestore
 import json
 import re
 
-pattern = r'^(\d+(?:\.(?:\d+)?\.?(?:\w+)?){0,5}) (.+?)(?=\n\n|\n$)'
 cred = credentials.Certificate('firebase-key.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.Client()
 rulesRef = db.collection('core_rules')
 
-rules = []
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dry', help='should this be a test run or not?', type=bool, default=True)
 
+args = parser.parse_args()
 filename = 'core-rules.txt'
 with open(filename, 'r', encoding='utf8') as f:
     data = f.read()
     lines = data.split('\n')
+    pattern = r'^(\d+(?:\.(?:\d+)?\.?(?:\w+)?){0,5}) (.+?)(?=\n\n|\n$)'
     matches = re.findall(pattern, data, flags=re.DOTALL | re.MULTILINE)
+
     for match in matches:
         number = match[0]
         text = match[1]
-        rule = {
-            'ruleNumber': number,
-            'text': text
-        }
-        rulesRef.document(number).set(rule)
+
+        if (args.dry):
+            print(f'{number}: {text}')
+        else:
+            rule = {
+                'ruleNumber': number,
+                'text': text
+            }
+            rulesRef.document(number).set(rule)
+
+    if (args.dry):
+        print('==========')
+        print(f'Found {len(matches)} rules')
