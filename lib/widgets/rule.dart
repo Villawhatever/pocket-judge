3783681c3272877f80a3460d10eb35aceaf4b916
Villawhatever <andrew@primefour.com>
2026-04-04 00:47:18 -0500

    Version bump

--- lib/widgets/rule.dart
+++ lib/widgets/rule.dart
@@ -68,7 +68,7 @@ class RuleWidget extends StatelessWidget {
     fragments.add(TextSpan(text: model.text.substring(currentPosition)));
 
     final double leftPadding = shouldIndent
-        ? math.max(15 * (countCharacters(model.number, '.') - 1), 0)
+        ? math.max(20 * (countCharacters(model.number, '.') - 1), 0)
         : 0;
 
     return Padding(
