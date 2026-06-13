77be5af7c22b1672e3576aa370cfc9c5712d0e23
Villawhatever <andrew@primefour.com>
2026-06-13 15:15:17 -0500

    Minor: Tweak FAQs searching.

--- lib/errata/erratum_widget.dart
+++ lib/errata/erratum_widget.dart
@@ -6,8 +6,11 @@ import '../utils/extensions/context_extensions.dart';
 import 'erratum.dart';
 
 class ErratumWidget extends StatefulWidget {
-  const ErratumWidget(
-      {super.key, required this.model, required this.expansibleController});
+  const ErratumWidget({
+    super.key,
+    required this.model,
+    required this.expansibleController,
+  });
 
   final ErratumModel model;
   final ExpansibleController expansibleController;
@@ -29,10 +32,11 @@ class _ErratumWidgetState extends State<ErratumWidget> {
   Widget build(BuildContext context) {
     return Expansible(
       headerBuilder: (context, animation) => ExpansibleHeader(
-          title: widget.model.name,
-          context: context,
-          animation: animation,
-          expansibleController: _expansibleController),
+        title: widget.model.name,
+        context: context,
+        animation: animation,
+        expansibleController: _expansibleController,
+      ),
       bodyBuilder: (context, animation) {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -46,14 +50,18 @@ class _ErratumWidgetState extends State<ErratumWidget> {
                 child: Column(
                   children: [
                     Text('NEW TEXT', style: context.textTheme.titleMedium),
-                    Text(widget.model.newText!,
-                        style: context.textTheme.bodyMedium)
+                    Text(
+                      widget.model.newText!,
+                      style: context.textTheme.bodyMedium,
+                    ),
                   ],
                 ),
               ),
               Center(
-                child: FaIcon(FontAwesomeIcons.chevronUp,
-                    color: context.colorScheme.secondary),
+                child: FaIcon(
+                  FontAwesomeIcons.chevronUp,
+                  color: context.colorScheme.secondary,
+                ),
               ),
               Container(
                 padding: EdgeInsetsGeometry.fromLTRB(5, 12, 5, 12),
@@ -64,68 +72,79 @@ class _ErratumWidgetState extends State<ErratumWidget> {
                 child: Column(
                   children: [
                     Text('OLD TEXT', style: context.textTheme.titleMedium),
-                    Text(widget.model.oldText!,
-                        style: context.textTheme.bodyMedium)
+                    Text(
+                      widget.model.oldText!,
+                      style: context.textTheme.bodyMedium,
+                    ),
                   ],
                 ),
               ),
               if (widget.model.faqs?.isNotEmpty ?? false)
                 const Divider(
-                    height: 25,
-                    thickness: 3,
-                    indent: 25,
-                    endIndent: 25,
-                    color: Colors.white),
+                  height: 25,
+                  thickness: 3,
+                  indent: 25,
+                  endIndent: 25,
+                  color: Colors.white,
+                ),
             ],
             if (widget.model.faqs?.isNotEmpty ?? false)
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-                if (widget.model.set != "UNL")
-                  Container(
-                    decoration: BoxDecoration(
-                      color: context.colorScheme.secondaryContainer,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Padding(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  if (widget.model.set != 'UNL')
+                    Container(
+                      decoration: BoxDecoration(
+                        color: context.colorScheme.secondaryContainer,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Padding(
                         padding: EdgeInsetsGeometry.fromLTRB(12, 5, 12, 5),
                         child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Flexible(
-                                  child: FaIcon(
-                                      FontAwesomeIcons.triangleExclamation)),
-                              Flexible(
-                                  flex: 10,
-                                  child: Text(
-                                    'Heads up! This FAQ is not from the most recent set. Information may be outdated.',
-                                    textAlign: TextAlign.center,
-                                  )),
-                              Flexible(
-                                  child: FaIcon(
-                                      FontAwesomeIcons.triangleExclamation)),
-                            ])),
-                  ),
-                for (final faq in widget.model.faqs!) ...[
-                  if (faq.question?.isNotEmpty ?? false)
-                    Text(faq.question!,
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  Padding(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Flexible(
+                              child: FaIcon(
+                                FontAwesomeIcons.triangleExclamation,
+                              ),
+                            ),
+                            Flexible(
+                              flex: 10,
+                              child: Text(
+                                'Heads up! This FAQ is not from the most recent set. Information may be outdated.',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Flexible(
+                              child: FaIcon(
+                                FontAwesomeIcons.triangleExclamation,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  for (final faq in widget.model.faqs!) ...[
+                    if (faq.question?.isNotEmpty ?? false)
+                      Text(
+                        faq.question!,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    Padding(
                       padding: EdgeInsetsGeometry.only(bottom: 12),
-                      child: Text(faq.answer!))
-                ]
-              ]),
+                      child: Text(faq.answer!),
+                    ),
+                  ],
+                ],
+              ),
           ],
         );
       },
       controller: _expansibleController,
-      expansibleBuilder: (
-        context,
-        header,
-        body,
-        animation,
-      ) =>
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [header, body]),
+      expansibleBuilder: (context, header, body, animation) => Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [header, body],
+      ),
     );
   }
 }
--- lib/nav/menu.dart
+++ lib/nav/menu.dart
@@ -8,8 +8,11 @@ import '../simple_views/about_view.dart';
 import '../utils/extensions/context_extensions.dart';
 
 class LinkGenerator {
-  const LinkGenerator(
-      {required this.name, required this.img, required this.view});
+  const LinkGenerator({
+    required this.name,
+    required this.img,
+    required this.view,
+  });
 
   final String name;
   final String img;
@@ -23,50 +26,61 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     var linkBuilders = [
       LinkGenerator(
-          name: 'Core Rules',
-          img: 'lib/assets/img/core_rules.png',
-          view: CoreRulesView(title: 'Core Rules')),
+        name: 'Core Rules',
+        img: 'lib/assets/img/core_rules.png',
+        view: CoreRulesView(title: 'Core Rules'),
+      ),
       LinkGenerator(
-          name: 'Tournament Rules',
-          img: 'lib/assets/img/tournament_rules.png',
-          view: TournamentRulesView(title: 'Tournament Rules')),
+        name: 'Tournament Rules',
+        img: 'lib/assets/img/tournament_rules.png',
+        view: TournamentRulesView(title: 'Tournament Rules'),
+      ),
       LinkGenerator(
-          name: 'Card Search',
-          img: 'lib/assets/img/card_search.png',
-          view: CardSearchView(title: 'Card Search')),
+        name: 'Card Search',
+        img: 'lib/assets/img/card_search.png',
+        view: CardSearchView(title: 'Card Search'),
+      ),
       LinkGenerator(
-          name: 'Card Specific Notes',
-          img: 'lib/assets/img/card_notes.png',
-          view: ErrataView(title: 'Card Specific Notes')),
+        name: 'Card Specific Notes',
+        img: 'lib/assets/img/card_notes.png',
+        view: ErrataView(title: 'Card Specific Notes'),
+      ),
       LinkGenerator(
-          name: 'About',
-          img: 'lib/assets/img/about.png',
-          view: AboutView(title: 'About')),
+        name: 'About',
+        img: 'lib/assets/img/about.png',
+        view: AboutView(title: 'About'),
+      ),
     ];
 
     var links = [];
     for (final item in linkBuilders) {
-      links.add(ListTile(
+      links.add(
+        ListTile(
           leading: item.name == 'Card Specific Notes'
               ? Transform.translate(
                   offset: Offset(2, 0),
                   child: Image.asset(item.img, width: 32, height: 32),
                 )
               : Image.asset(item.img, width: 32, height: 32),
-          title: Text(item.name.toUpperCase(),
-              style: context.textTheme.titleMedium),
+          title: Text(
+            item.name.toUpperCase(),
+            style: context.textTheme.titleMedium,
+          ),
           onTap: () {
             Navigator.pushReplacement(
               context,
               MaterialPageRoute(builder: (context) => item.view),
             );
-          }));
+          },
+        ),
+      );
     }
 
     return Drawer(
-        child: ListView(
-      padding: MediaQuery.of(context).viewPadding,
-      children: [...links],
-    ));
+      child: ListView(
+        padding: MediaQuery.of(context).viewPadding,
+        children: [...links],
+      ),
+    );
   }
 }
